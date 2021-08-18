dad8fe0f7fc39dbf19369e5dba31a73c60bef18a
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-08-18 09:13:09 -0300

    removendo debug

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,11 @@ const config: Phaser.Types.Core.GameConfig = {
 	physics: {
 		default: 'matter',
 		matter: {
-			debug: {
-				boundsColor: 0xff0000,
-				lineColor: 0xff0000,
-				staticLineColor: 0xff0000
-			},
+			// debug: {
+			// 	boundsColor: 0xff0000,
+			// 	lineColor: 0xff0000,
+			// 	staticLineColor: 0xff0000
+			// },
 			gravity: { y: 0 },
 			setBounds: {
 				left: true,
